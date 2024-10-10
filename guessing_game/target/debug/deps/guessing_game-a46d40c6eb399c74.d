4d16078174dd2c703c50872b3998ba6e5b087398
Harshal <gahlotharshal@gmail.com>
2024-10-10 22:37:34 +0530

    first commit, First rust program, learn how to ask for input in rust.

---
+++ guessing_game/target/debug/deps/guessing_game-a46d40c6eb399c74.d
@@ -0,0 +1,5 @@
+/mnt/h/PROGRAMMING/Rust/Rust book projects/guessing_game/target/debug/deps/guessing_game-a46d40c6eb399c74: src/main.rs
+
+/mnt/h/PROGRAMMING/Rust/Rust book projects/guessing_game/target/debug/deps/guessing_game-a46d40c6eb399c74.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/mnt/h/PROGRAMMING/Rust/Rust\ book\ projects/guessing_game/target/debug/guessing_game: /mnt/h/PROGRAMMING/Rust/Rust\ book\ projects/guessing_game/src/main.rs
